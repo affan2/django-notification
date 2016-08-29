bf68191bf7d83144f23d79c99635b2ac84558875
Ahmed Salman <ahmed.salman.noor@gmail.com>
2016-08-29 13:35:38 +0500

    admin for notice setttings

--- notification/admin.py
+++ notification/admin.py
@@ -10,6 +10,7 @@ class NoticeTypeAdmin(admin.ModelAdmin):
 
 class NoticeSettingAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "notice_type", "medium", "send"]
+    search_fields = ('user__first_name', 'notice_type__label', )
 
 
 class NoticeAdmin(admin.ModelAdmin):
