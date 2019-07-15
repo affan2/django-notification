6faf91a94dc5b0ae868aa641830235946237ab9d
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-07-15 14:20:42 +0300

    Remove old style directories

--- pinax/notifications/compat.py
+++ pinax/notifications/compat.py
@@ -3,10 +3,7 @@ try:
 except ImportError:
     from urllib.parse import quote  # noqa
 
-try:
-    from threading import get_ident
-except ImportError:
-    from thread import get_ident  # noqa
+from threading import get_ident
 
 try:
     from account.decorators import login_required
