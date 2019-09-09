3f3d41d8676e6a85c90117a1d003f59aba69a058
Mohammad Abouchama <mohammadabouchama@gmail.com>
2019-09-09 12:18:10 +0400

    Fix bad import in models

--- pinax/notifications/tests/models.py
+++ pinax/notifications/tests/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth import get_user_model
 
 
 class Language(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     language = models.CharField("language", max_length=10)
