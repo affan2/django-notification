b28af5e86e021bc34dc3bedc77e8291d2b689c80
Mohammed Affan <affan2@gmail.com>
2019-10-06 16:03:02 +0400

    increased field size due to data

--- pinax/notifications/models.py
+++ pinax/notifications/models.py
@@ -37,7 +37,7 @@ class NoticeType(models.Model):
     label = models.CharField(_("label"), max_length=40)
     display = models.CharField(_("display"), max_length=50)
     past_tense = models.CharField(_("Past Tense"), max_length=100)
-    description = models.CharField(_("description"), max_length=100)
+    description = models.CharField(_("description"), max_length=250)
 
     # by default only on for media with sensitivity less than or equal to this number
     default = models.IntegerField(_("default"))
