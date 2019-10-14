d9f3864c8fb586f5db4e5d9cd390fe4f6f741053
Mohammed Affan <affan2@gmail.com>
2019-10-14 19:54:10 +0400

    is_authenticated is a boolean

--- pinax/notifications/context_processors.py
+++ pinax/notifications/context_processors.py
@@ -4,7 +4,7 @@ from .models import Notice
 
 
 def notification(request):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         return {
             "notice_unseen_count": Notice.objects.unseen_count_for(request.user, on_site=True),
             "notifications": Notice.objects.filter(recipient=request.user.id, site_id=settings.SITE_ID)
